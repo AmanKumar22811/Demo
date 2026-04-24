32391d93ff52dd3bb521fc2a49ef2cc74b8442d8
aayushi1139 <lalitahaj@gmail.com>
2026-04-24 14:56:35 +0530

    Controller 3

--- src/main/java/com/cg/controller/Controller.java
+++ src/main/java/com/cg/controller/Controller.java
@@ -1,8 +1,13 @@
 package com.cg.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
+	@GetMapping("Controller3")
+	public String controller3() {
+		return "Hello from controller3";
+	}
 	
 }
