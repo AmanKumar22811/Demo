aa9aa6c04fbe20b823393d7c7f39225532b7f8a8
AmanKumar <aman228115918@gmail.com>
2026-04-24 14:18:31 +0530

    controller1 added

--- src/main/java/com/cg/controller/Controller.java
+++ src/main/java/com/cg/controller/Controller.java
@@ -1,8 +1,12 @@
 package com.cg.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Controller {
-	
+	@GetMapping("/controller1")
+	public String controller1() {
+		return "Hello from controller1";
+	}
 }
